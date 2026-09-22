44e426fe5a11245fac7bb718e7d9cca54e5cdfe6
elliottdecary5559-a11y <elliott.decary5559@gmail.com>
2026-09-22 10:55:25 -0400

    index.js

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ client.once('ready', () => {
   console.log(`🤖 Bot connecté sous le nom de : ${client.user.tag}`);
 });
 
-client.login('MTU1MTk1NjE5NzQwNDMxMTU1Mg.Gp-4-8.Y485uvanhfOvlwrNSPy39h2nuCJyYWSu-61-Ek');
+client.login('MTU1MTk1NjE5NzQwNDMxMTU1Mg.GsQLKS.FCIqEXJugJIzXZu6SczaPxNiiSM7J4yfrNzS1I');
