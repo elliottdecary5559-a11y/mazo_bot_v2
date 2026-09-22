40fea9b0415d6aeea2579ba7511825dfb77e4599
elliottdecary5559-a11y <elliott.decary5559@gmail.com>
2026-09-22 10:57:32 -0400

    index.js

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ client.once('ready', () => {
   console.log(`🤖 Bot connecté sous le nom de : ${client.user.tag}`);
 });
 
-client.login('MTU1MTk1NjE5NzQwNDMxMTU1Mg.GsQLKS.FCIqEXJugJIzXZu6SczaPxNiiSM7J4yfrNzS1I');
+// Le bot va lire le token depuis les variables sécurisées
+client.login(process.env.DISCORD_TOKEN);
